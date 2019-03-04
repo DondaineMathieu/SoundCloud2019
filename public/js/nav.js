f30ac539496e156ff9ea8f4ed51e7ee64fff1428
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-03-04 09:48:01 +0100

    ajout script pour les catégories

---
+++ public/js/nav.js
@@ -0,0 +1,10 @@
+// document.getElementById("categories").onclick = test();
+
+function test() {
+    if (document.getElementById("ul-categories").style.display == "flex") {
+        document.getElementById("ul-categories").style.display = "none";
+    } else {
+        document.getElementById("ul-categories").style.display = "flex";
+    }
+    
+}
