1716b99304b44a39fefe57ed300cd5c4999db227
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-03-20 08:32:23 +0100

    numerotation musiques

--- resources/views/_chansons.blade.php
+++ resources/views/_chansons.blade.php
@@ -1,15 +1,17 @@
 <table class="table text-center">
     <thead class="thead-dark">
         <tr>
-            <th scope="col">Ecoutes</th>
+            <th scope="col">#</th>
             <th scope="col">Musique</th>
             <th scope="col">Auteur</th>
         </tr>
     </thead>
     <tbody>
+    <div style="display:none;">{{$i=0}}</div>
     @foreach ($chansons as $c)
+    <div style="display:none;">{{$i=$i+1}}</div>
         <tr>
-            <th scope="row">{{$c->id}}</th>
+            <th scope="row">{{$i}}</th>
             <td><a href="#" class="chanson" data-file="{{$c->fichier}}" data-name="{{$c->nom}}" data-user="{{$c->utilisateur->name}}">{{$c->nom}}</a></td>
             <td><a href="/utilisateur/{{$c->utilisateur->id}}">{{$c->utilisateur->name}}</td>
         </tr>
